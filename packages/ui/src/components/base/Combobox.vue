71a868b5c2b1bfcf89a93c3339a52524778c9243
Creeperkatze <contact@creeperkatze.dev>
2026-05-12 11:20:10 +0200

    Remove unnecessary styling

--- packages/ui/src/components/base/Combobox.vue
+++ packages/ui/src/components/base/Combobox.vue
@@ -62,20 +62,18 @@
 			@click="handleTriggerClick($event)"
 			@keydown="handleTriggerKeydown"
 		>
-			<div class="flex min-w-0 items-center gap-2">
-				<span class="shrink-0 whitespace-nowrap">
-					<slot name="prefix"></slot>
-				</span>
+			<div class="flex items-center gap-2">
+				<slot name="prefix"></slot>
 				<component
 					:is="selectedOption?.icon"
 					v-if="showIconInSelected && selectedOption?.icon"
-					class="h-5 w-5 shrink-0"
+					class="h-5 w-5"
 				/>
-				<span class="min-w-0 truncate whitespace-nowrap text-primary font-semibold leading-tight">
+				<span class="text-primary font-semibold leading-tight">
 					<slot name="selected">{{ triggerText }}</slot>
 				</span>
 			</div>
-			<div class="flex shrink-0 items-center gap-1">
+			<div class="flex items-center gap-1">
 				<slot name="suffix"></slot>
 				<ChevronLeftIcon
 					v-if="showChevron"
