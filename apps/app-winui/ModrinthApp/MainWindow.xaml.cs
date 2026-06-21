aca8d8fbb33f8ed84b9aa22eab482e5c3ced4463
Creeperkatze <contact@creeperkatze.dev>
2026-06-21 10:17:59 +0200

    Fix navbat

--- apps/app-winui/ModrinthApp/MainWindow.xaml.cs
+++ apps/app-winui/ModrinthApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
 using ModrinthApp.Pages;
 
 namespace ModrinthApp;
@@ -9,6 +10,10 @@ public sealed partial class MainWindow : Window
 	public MainWindow()
 	{
 		this.InitializeComponent();
+
+		// Extend client area into the title bar so Mica shows behind our custom bar.
+		this.ExtendsContentIntoTitleBar = true;
+		this.SetTitleBar(AppTitleBar);
 	}
 
 	private void NavView_Loaded(object sender, RoutedEventArgs e)
@@ -39,5 +44,35 @@ public sealed partial class MainWindow : Window
 
 		if (pageType is not null && (ContentFrame.CurrentSourcePageType != pageType))
 			ContentFrame.Navigate(pageType);
+	}
+
+	private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+	{
+		BackButton.IsEnabled    = ContentFrame.CanGoBack;
+		ForwardButton.IsEnabled = ContentFrame.CanGoForward;
+
+		PageTitleText.Text = e.SourcePageType?.Name switch
+		{
+			nameof(HomePage)     => "Home",
+			nameof(LibraryPage)  => "Library",
+			nameof(BrowsePage)   => "Browse",
+			nameof(WorldsPage)   => "Worlds",
+			nameof(SkinsPage)    => "Skins",
+			nameof(SettingsPage) => "Settings",
+			nameof(InstancePage) => "Instance",
+			_                    => string.Empty,
+		};
+	}
+
+	private void BackButton_Click(object sender, RoutedEventArgs e)
+	{
+		if (ContentFrame.CanGoBack)
+			ContentFrame.GoBack();
+	}
+
+	private void ForwardButton_Click(object sender, RoutedEventArgs e)
+	{
+		if (ContentFrame.CanGoForward)
+			ContentFrame.GoForward();
 	}
 }
